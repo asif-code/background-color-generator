afa2ffca7b32c1dbdff8225200438d0d937040f2
Asif Ali <yoasif0708@gmail.com>
2022-10-05 16:23:23 +0530

    Adding node moduels and Browserify

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var _ = require("lodash");
+
+var array = [1, 2, 3, 4, 5, 6];
+console.log("Answer:", _.without(array, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
